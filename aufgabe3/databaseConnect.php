b15c1e631d6785e9602f8590ed97fae878b064ec
Georg <georg.schmidhammer@student.uibk.ac.at>
2013-04-19 11:43:15 +0200

    Passwort

--- aufgabe3/databaseConnect.php
+++ aufgabe3/databaseConnect.php
@@ -8,7 +8,7 @@ class DatabaseConnect{
 	
 	public function __construct(){
 		if(!isset($this->mysqli)){
-			$this->mysqli = new mysqli("localhost", "root", "", "wiki");
+			$this->mysqli = new mysqli("localhost", "root", "new-password", "wiki");
 			if ($this->mysqli->connect_errno) {
 				echo "Failed to connect to MySQL: " . $this->mysqli->connect_error;
 			}
